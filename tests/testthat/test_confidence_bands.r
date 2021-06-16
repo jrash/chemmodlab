1d22be7084d8553844ec60db6274274c9ac1b64c
Jeremy Ash <jrash@ncsu.edu>
2021-06-16 19:59:15 -0400

    Update some of the test code

--- tests/testthat/test_confidence_bands.r
+++ tests/testthat/test_confidence_bands.r
@@ -12,8 +12,8 @@ test_that("PerfCurveBands pointwise confidence intervals match those computed fo
   # Simulate Case 1 scores from paper
   r <- c(2^seq(1, 13), 3^seq(1, 8), 10, 105, 300, 1500, 15000)
   r <- r[order(r)]/150000
-
   m <- 150000
+
   pi.0.true <- 1/501
   set.seed(567)
   X <- rbinom(n=m, size=1, prob=pi.0.true)
@@ -24,6 +24,10 @@ test_that("PerfCurveBands pointwise confidence intervals match those computed fo
   method.v <- c("binomial", "binomial", "JZ", "JZ", "bootstrap", "bootstrap")
   correction.v <- c(F, T, F, T, F, T)
 
+  r.all <- (1:m)/m
+  idx <- which(r.all %in% r)
+  k.ide <- (cumsum(rev(sort(X)))[idx]/sum(X))
+  
   for(i in 1:6) {
     
     # Test recall
@@ -31,8 +35,10 @@ test_that("PerfCurveBands pointwise confidence intervals match those computed fo
                              plus2 = correction.v[i], conf.level = .95, boot.rep = 100, myseed = 567)
     
     exp_ci <- k.ls[[i]]$interval.ls[[1]]
+    exp_ci[, 1] <- ifelse(exp_ci[, 1] < 0, 0, exp_ci[, 1])
+    exp_ci[, 2] <- ifelse(exp_ci[, 2] > k.ide, k.ide, exp_ci[, 2])
     
-    expect_equal(act_ci, exp_ci, check.attribute = F)
+    expect_equal(act_ci$CI, exp_ci, check.attribute = F)
     
     # Test precision
     act_ci <- PerfCurveBands(S, X, r, metric = "prec", method = method.v[i], type = "pointwise",
@@ -40,8 +46,10 @@ test_that("PerfCurveBands pointwise confidence intervals match those computed fo
     
     
     exp_ci <- pi.ls[[i]]$interval.ls[[1]]
+    exp_ci[, 1] <- ifelse(exp_ci[, 1] < 0, 0, exp_ci[, 1])
+    exp_ci[, 2] <- ifelse(exp_ci[, 2] > 1, 1, exp_ci[, 2])
     
-    expect_equal(act_ci, exp_ci, check.attribute = F)
+    expect_equal(act_ci$CI, exp_ci, check.attribute = F)
     
   }
   
@@ -80,8 +88,10 @@ test_that("PerfCurveBands confidence bands match those computed for Case 1 in th
                              mc.rep = 100000, myseed = 111)
     
     exp_ci <- k.ls[[i]]$interval.ls[[1]]
+    exp_ci[, 1] <- ifelse(exp_ci[, 1] < 0, 0, exp_ci[, 1])
+    exp_ci[, 2] <- ifelse(exp_ci[, 2] > k.ide, k.ide, exp_ci[, 2])
     
-    expect_equal(act_ci, exp_ci, check.attribute = F)
+    expect_equal(act_ci$CI, exp_ci, check.attribute = F)
     
     # Test precision
     act_ci <- PerfCurveBands(S, X, r, metric = "prec", method = method.v[i], type = "band",
@@ -90,9 +100,21 @@ test_that("PerfCurveBands confidence bands match those computed for Case 1 in th
     
     
     exp_ci <- pi.ls[[i]]$interval.ls[[1]]
+    exp_ci[, 1] <- ifelse(exp_ci[, 1] < 0, 0, exp_ci[, 1])
+    exp_ci[, 2] <- ifelse(exp_ci[, 2] > 1, 1, exp_ci[, 2])
     
-    expect_equal(act_ci, exp_ci, check.attribute = F)
+    expect_equal(act_ci$CI, exp_ci, check.attribute = F)
     
   }
   
 })
+
+
+plot(r, act_ci$rec, type = "l")
+lines(r, act_ci$CI[, 1], type = "l", col = "red")
+lines(r, act_ci$CI[, 2], type = "l", col = "red")
+
+
+plot(r, act_ci$prec, type = "l")
+lines(r, act_ci$CI[, 1], type = "l", col = "red")
+lines(r, act_ci$CI[, 2], type = "l", col = "red")
